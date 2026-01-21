b7859f859c92ccc6bcd08522995bf5d1e31c6625
searoopy <searoopy@gmail.com>
2026-01-21 10:05:01 +0900

    포인터 메모리 릭 검사 예제 진행
    -crt 라이어브러리를 활용해 메모리 릭 검사하는 예제ddfdfdf

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifdef _DEBUG
+#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define DBG_NEW new
+#endif
 
 void DeletePointer(void*& pointer)
 {
@@ -12,12 +23,21 @@ void DeletePointer(void*& pointer)
 int main()
 {
 
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetBreakAlloc(158);
+
+
+	// todo : 메로리 릭 일부러
+	int* intpointer = new int;
+	delete intpointer
+
+	//new int[10];
 
 	//동적 할당
-	char* buffer = new char[10];
+	//char* buffer = new char[10];
 
-	char* buffer1 = buffer;
-	char** buffer2 = &buffer;
+	//char* buffer1 = buffer;
+	//char** buffer2 = &buffer;
 
 
 
